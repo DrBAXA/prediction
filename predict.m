98fd888614bfb04bd42b4a8048de3b61319b4085
DrBAXA <DrBAXA@hotmail.com>
2016-03-17 00:36:08 +0200

    J(theta) = 2.4521e+07

--- predict.m
+++ predict.m
@@ -1,7 +1,7 @@
 function theta = predict()
 data = load("out.csv");
 y = data(:, 1);
-X = data(:, [2:14]);
+X = data(:, [2:size(data, 2)]);
 X = [ones(size(X, 1), 1) X];
 theta = pinv(X'*X)*X'*y;
 end
--- test.m
+++ test.m
@@ -1,10 +1,11 @@
-function [y, delta] = test(theta)
+function [y, delta, j] = test(theta)
 data = load("out.csv");
 y = data(:, 1);
-X = data(:, [2:14]);
+X = data(:, [2:size(data, 2)]);
 X = [ones(size(X, 1), 1) X];
 x = X*theta;
 t = 1:size(y, 1);
 plot(t, y, "xr", t, x, "g");
 delta = y-x;
+j = sum(delta.^2);
 end
